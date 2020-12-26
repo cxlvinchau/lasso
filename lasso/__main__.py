848bae5dffbb9524640780cfe91ca90399632765
Calvin <calvinchau@outlook.de>
2020-12-26 19:11:40 +0100

    added pctl

--- lasso/__main__.py
+++ lasso/__main__.py
@@ -1,4 +1,5 @@
 from lasso.dtmc import DTMC
+from lasso.pctl.formulae import Atomic
 
 
 def main():
@@ -12,6 +13,13 @@ def main():
     dtmc.add_transition(s3,1,s1)
     dtmc.steady_state()
     dtmc.visualize()
+    a1,a2 = Atomic("a1",dtmc),Atomic("a2",dtmc)
+    a1.add_states(s1,s2)
+    a2.add_states(s3)
+    f = (-(a1))
+    f.eval()
+    print(f.states)
+
 
 
 if __name__ == "__main__":
--- lasso/pctl/formulae.py
+++ lasso/pctl/formulae.py
@@ -1,4 +1,78 @@
-class Formula:
+class StateFormula:
 
-    def __init__(self):
+    def __init__(self,dtmc):
+        """Super class for state formulae"""
+        # States that satisfy this formula
+        self.all = set(dtmc.states)
+        self.states = set()
+        self.dtmc = dtmc
+
+    def eval(self):
+        """Needs to be overridden in child class and call eval on children"""
         pass
+
+    def __and__(self, other):
+        if isinstance(other,StateFormula):
+            return Conjunction(self,other,self.dtmc)
+
+    def __or__(self, other):
+        if isinstance(other,StateFormula):
+            return Disjunction(self,other,self.dtmc)
+
+    def __neg__(self):
+        return Negation(self,self.dtmc)
+
+    def __str__(self):
+        pass
+
+
+class Atomic(StateFormula):
+
+    def __init__(self,label,dtmc):
+        super().__init__(dtmc)
+        self.label = label
+
+    def add_states(self,*args):
+        for s in args:
+            self.states.add(s)
+
+    def __str__(self):
+        return self.label
+
+
+class Conjunction(StateFormula):
+    """Conjunction state formula"""
+    def __init__(self,f1,f2,dtmc):
+        super().__init__(dtmc)
+        self.f1 = f1
+        self.f2 = f2
+
+    def eval(self):
+        self.f1.eval()
+        self.f2.eval()
+        self.states = self.f1.states.intersection(self.f2.states)
+
+
+class Disjunction(StateFormula):
+    """Disjunction state formula"""
+    def __init__(self,f1,f2,dtmc):
+        super().__init__(dtmc)
+        self.f1 = f1
+        self.f2 = f2
+
+    def eval(self):
+        self.f1.eval()
+        self.f2.eval()
+        self.states = self.f1.states.union(self.f2.states)
+
+
+class Negation(StateFormula):
+    """Negation state formula"""
+    def __init__(self,f,dtmc):
+        super().__init__(dtmc)
+        self.f = f
+
+    def eval(self):
+        self.f.eval()
+        self.states = self.dtmc.states - self.f.states
+
